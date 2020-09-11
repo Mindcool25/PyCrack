5471f3139e34d3c03945ef7830b41d758bf98a56
Mindcool225 <thezachm24@gmail.com>
2020-09-11 16:24:12 +0000

    Imported Colorama and defined colors for output

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 # imports
 from modules import wifi
+from colorama import Fore as color
 
 # initializing variables
 
+# initializing colors
+GREEN = color.GREEN
+RED = color.RED
+YELLOW = "\033[93m"
+WHITE = color.WHITE
+
+# banner
+banner = '''
+Banner here later
+'''
 
 # main code
 
-wifi.test()
